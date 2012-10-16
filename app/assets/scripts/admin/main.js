100fbf588c6e01ec4c71efd0a05d495b2ffa7f08
Jim Posen <jim.posen@gmail.com>
2012-10-16 03:38:44 -0400

    Made navbar for admin pages.

--- app/assets/scripts/admin/main.js
+++ app/assets/scripts/admin/main.js
@@ -6,7 +6,7 @@ require(['cs!common/main','admin/crop','admin/delete-article',
          'admin/delete-image','admin/json-to-form','admin/k4export',
          'admin/layout','admin/newsletter','admin/nicedate',
          'cs!admin/form-field','cs!admin/taxonomy','admin/upload',
-         'cs!admin/page-layout'],
+         'cs!admin/page-layout', 'lib/bootstrap'],
         function (main) {
             main.apply(this, arguments);
         });
